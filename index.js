fe21c81d5964b53c3ae33352853e6981ce4c1b26
Jagantech2020 <jagantech2020@gmail.com>
2022-06-16 23:29:39 +0530

    version update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const isString = (val) => !!(typeof val === "string");
 const isNotEmpty = (val) => !!(val && val.trim().length > 0);
-const stringhelper = { isString, isNotEmpty };
+const toUpperCase = (val) => val && typeof val === "string" ? val.toUpperCase() : val;
+const stringhelper = { isString, isNotEmpty, toUpperCase};
 module.exports =  stringhelper;
